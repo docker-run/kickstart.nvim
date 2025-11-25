0d2446fb7851dce0f0789fd81e4d9b391b4dc12c
Maksym Kordiyak <maksym.kordiyak@safello.com>
2025-11-25 13:03:06 +0100

    Update dependencies. Fix leap.nvim warnings about deprecated mappings

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,6 +1,9 @@
 return {
   'ggandor/leap.nvim',
   config = function()
-    require('leap').set_default_mappings()
+    -- Use the new recommended setup instead of deprecated set_default_mappings()
+    -- 's' for leap forward, 'S' for leap from any window
+    vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
   end,
 }
