80f53cb635a355c4dec72d395aaa42c4c1deebc6
docker-run <maksym.kordiyak.work@gmail.com>
2026-01-18 18:25:55 +0100

    Cleanup lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,18 +3,28 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
-      options = { theme = 'auto' },
+      options = { theme = 'auto', refresh = {
+        statusline = 100,
+        tabline = 100,
+        winbar = 100,
+      } },
       sections = {
         lualine_a = { { 'mode', upper = true } },
         lualine_b = { { 'branch', icon = '' } },
         lualine_c = {
-          { 'filename', path = 1, file_status = true },
-          require('lsp-progress').progress,
-          'g:metals_status',
+          { 'filename', file_status = true },
         },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
       },
       extensions = { 'fzf' },
     }
