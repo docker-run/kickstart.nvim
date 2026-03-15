3a7dbf90aee5cae9c27c264262ba0ddd27b4c5b4
docker-run <maksym.kordiyak.work@gmail.com>
2026-03-15 14:10:59 +0100

    Fix leap.lua repo url change. update deps

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,9 +1,9 @@
 return {
-  'ggandor/leap.nvim',
+  url = 'https://codeberg.org/andyg/leap.nvim',
   config = function()
     -- Use the new recommended setup instead of deprecated set_default_mappings()
     -- 's' for leap forward, 'S' for leap from any window
-    vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
     vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
   end,
 }
